2538a3ec8fe2cb8a9033c9c3c43a6d12fc83593d
ViktorGreg <limviktorgreg14@gmail.com>
2026-09-19 04:57:57 +0800

    Created the design on Admin.
    Added dashboard.html
    
    Note: This is only a design with no backend functionality yet. That will be continue.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
